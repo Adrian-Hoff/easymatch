629419bee2c0bff29227d7c546d3d526358b4e31
Adrian Hoff <adrian.hoff2005@gmail.com>
2024-03-08 14:58:34 -0300

    fix: fix dependencies conflicts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { useFonts } from 'expo-font'
+import { Center, NativeBaseProvider, Spinner, StatusBar } from 'native-base'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { AuthProvider } from './src/contexts/AuthContext'
+import Routes from './src/routes'
+import theme from './src/theme'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  // const [fontsLoaded] = useFonts({
+  //   Poppins_400Regular: require('@assets/fonts/Poppins/Poppins-Regular.ttf'),
+  //   Poppins_500Medium: require('@assets/fonts/Poppins/Poppins-Medium.ttf'),
+  //   Poppins_600SemiBold: require('@assets/fonts/Poppins/Poppins-SemiBold.ttf'),
+  //   Poppins_700Bold: require('@assets/fonts/Poppins/Poppins-Bold.ttf'),
+  // })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <NativeBaseProvider theme={theme}>
+      <AuthProvider>
+        <StatusBar
+          translucent
+          backgroundColor={'transparent'}
+          barStyle={'dark-content'}
+        />
+        <SafeAreaView style={{ flex: 1 }}>
+            <Routes />
+        </SafeAreaView>
+      </AuthProvider>
+    </NativeBaseProvider>
+  )
+}
